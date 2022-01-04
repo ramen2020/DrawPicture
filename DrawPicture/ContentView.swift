e23b31a810a0020d52f29111558270489ce3438f
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-01-05 08:23:25 +0900

    init

---
+++ DrawPicture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrawPicture
+//
+//  Created by 宮本光直 on 2022/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
