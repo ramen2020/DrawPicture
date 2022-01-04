cf9a662efde44334ab267e88c9957394a9bc0ee0
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-01-05 08:23:30 +0900

    init

---
+++ DrawPicture/DrawPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawPictureApp.swift
+//  DrawPicture
+//
+//  Created by 宮本光直 on 2022/01/05.
+//
+
+import SwiftUI
+
+@main
+struct DrawPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrawPictureUITests/DrawPictureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DrawPictureUITestsLaunchTests.swift
+//  DrawPictureUITests
+//
+//  Created by 宮本光直 on 2022/01/05.
+//
+
+import XCTest
+
+class DrawPictureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
