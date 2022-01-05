8f5cb1b5094dd3d169d2c5b7de7c6f75b976fb56
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-01-06 07:45:45 +0900

    add custom paint view

--- DrawPicture/ContentView.swift
+++ DrawPicture/ContentView.swift
@@ -6,16 +6,81 @@
 //
 
 import SwiftUI
+import PencilKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                NavigationLink(destination: CustomPaint()) {
+                    Text("CustomPaint")
+                }
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct CustomPaint: View {
+    @State private var lines: [PaintLine] = []
+    @State private var currentLine: PaintLine?
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.white)
+                .gesture(
+                    DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                        .onChanged({ value in
+                            if currentLine == nil {
+                                currentLine = PaintLine.makeLine(points: [])
+                            }
+                            guard var line = currentLine else { return }
+                            line.points.append(value.location)
+                            currentLine = line
+                        })
+                        .onEnded({ value in
+                            guard var line = currentLine else { return }
+                            line.points.append(value.location)
+                            lines.append(line)
+                            currentLine = nil
+                        })
+                )
+            
+            // 書き終わったLineの描画
+            ForEach(lines) { line in
+                Path { path in
+                    path.addLines(line.points)
+                }.stroke(Color.red, lineWidth: 3)
+            }.clipped()
+            
+            // 今書いているline
+            Path { path in
+                guard let line = currentLine else { return }
+                path.addLines(line.points)
+            }.stroke(Color.red, lineWidth: 3)
+                .clipped()
+        }
+        .border(.gray, width: 3)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarTitle("手書き絵")
+        .navigationBarItems(trailing:
+                                Button(action: {
+            lines = []
+        }, label: {
+            Text("clear")
+        })
+        )
+    }
+}
+
+struct PaintLine: Identifiable {
+    static var idCount: Int = 0
+    var id: String
+    var points: [CGPoint]
+    
+    static func makeLine(points: [CGPoint]) -> PaintLine {
+        let line = PaintLine(id: "\(PaintLine.idCount)", points: points)
+        PaintLine.idCount += 1
+        return line
     }
 }
